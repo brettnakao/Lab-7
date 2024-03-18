72afc16c4258da0b46f348f7b4626df05e735f25
brettnakao <bnakao@lion.lmu.edu>
2024-03-18 13:08:02 -0700

    commit lab 7 examples

---
+++ Example 1- HIV Viral Load.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 10:18:48 2024
+
+@author: brettnakao
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Equation 1
+time = np.linspace(0, 10, 101)
+
+B = 175000
+A = 0
+alpha = 0
+beta = .5
+
+viral_load = np.empty(len(time))
+
+for i in range(len(time)):
+    viral_load[i] = A*np.exp(alpha*time[i]) + B*np.exp(-beta*time[i])
+
+# Experimental data points
+hiv_data = np.genfromtxt('HIVseries.csv', delimiter=",")
+x = hiv_data[:, 0]
+y = hiv_data[:, 1]
+
+# Plot
+plt.figure()
+
+plt.plot(time, viral_load, label='Function')
+
+plt.scatter(x, y, marker='+', label='Experimental Data')
+
+plt.xlabel('Time')
+plt.ylabel('Viral Load')
+plt.title('Viral Load vs. Time for an HIV-positive patient')
+plt.legend()
+
+plt.show
---
+++ Example 2- Bacterial Growth.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 12:35:56 2024
+
+@author: brettnakao
+"""
+# =============================================================================
+# import numpy as np
+# import matplotlib.pyplot as plt
+# 
+# t = np.linspace(0, 2, 20)
+# 
+# #W1
+# A = 1
+# tau = 1
+# 
+# W = np.empty(len(t))
+# 
+# for i in range(len(t)):
+#     W[i] = A*(np.exp(-t[i]/tau)-1+t[i]/tau)
+# 
+# #W2
+# A2 = 3
+# tau2 = .8
+# 
+# W2 = np.empty(len(t))
+# 
+# for i in range(len(t)):
+#     W2[i] = A2*(np.exp(-t[i]/tau2)-1+t[i]/tau2)
+# 
+# #W3
+# A3 = 7
+# tau3 = 5
+# 
+# W3 = np.empty(len(t))
+# 
+# for i in range(len(t)):
+#     W3[i] = A3*(np.exp(-t[i]/tau3)-1+t[i]/tau3)
+# 
+# # Plotting
+# plt.figure()
+# 
+# plt.plot(t, W, linestyle='solid', color='black', label='W1')
+# plt.plot(t, W2, linestyle='dashed', color='blue', label='W2')
+# plt.plot(t, W3, linestyle='dotted', color='green', label='W3')
+# 
+# plt.xlabel('Time')
+# plt.ylabel('W(t)')
+# plt.title('W(t)')
+# plt.legend()
+# 
+# plt.show
+# =============================================================================
+
+'''A)'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 7, 20)
+
+#V(t)
+tau = 3.3
+
+V = np.empty(len(t))
+
+for i in range(len(t)):
+    V[i] = 1-np.exp(-t[i]/tau)
+
+novickA_data = np.genfromtxt('g149novickA.csv', delimiter=",")
+x = novickA_data[:, 0]
+y = novickA_data[:, 1]
+
+# Plotting
+plt.figure()
+
+plt.plot(t, V, linestyle='solid', color='black', label='Curve-fit')
+
+plt.scatter(x, y, marker='+', label='Experimental Data')
+
+plt.xlabel('Time')
+plt.ylabel('V(t)')
+plt.title('V(t)')
+plt.legend()
+
+plt.show
+
+'''B)'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.linspace(0, 10, 20)
+
+A = .36
+tau = 10
+
+W = np.empty(len(time))
+
+for i in range(len(time)):
+    W[i] = A*(np.exp(-time[i]/tau)-1+time[i]/tau)
+
+novickB_data = np.genfromtxt('g149novickB.csv', delimiter=",")
+x1 = novickB_data[0:11, 0]
+y2 = novickB_data[0:11, 1]
+print(novickB_data)
+print(x1)
+
+# Plotting
+plt.figure()
+
+plt.plot(time, W, linestyle='solid', color='black', label='Curve-fit')
+
+plt.scatter(x1, y2, marker='+', label='Experimental Data')
+
+plt.xlabel('Time')
+plt.ylabel('W(t)')
+plt.title('W(t)')
+plt.legend()
+
+plt.show
